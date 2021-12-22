430bddad0400f74bbdf52d85407234a8afa01aa4
HassanBahati <mukisabahati@gmail.com>
2021-12-22 16:06:45 +0300

    lesson7

--- lesson7.py
+++ lesson7.py
@@ -16,3 +16,26 @@ mitsubishi = Car()
 ford = Car()
  
 print(nissan == toyota)
+
+
+# defining a class with properties, attributes or characters
+# in python , defining properties or structures of a an object we use __int__() method
+# RULE, self is the firest attribute of an object
+# self - keyword used to identify properties of a class 
+# name , color, model nd prices are parameters to be assigned to the propertiesof the class
+# init - intialising
+class Phone:
+    def __int__(self, name1, model1, color1, price1):
+        self.name = name1
+        self.model = model1
+        self.color = color1
+        self.price = price1
+
+
+tecno = Phone('tecno', 'camon15', 'blue', 700)
+samsung = Phone('sumsang', 'galaxyprime', 'white', 800)
+iphone = Phone('iphone', '12', 'grey', 1800)
+
+tecno.name = 2222
+print(tecno.name)
+# print(Phone())
